fe0be5a8dc0e2c31b267eb301228f1d55daa5974
Niraj <niraj.webtech@gmail.com>
2022-01-25 16:51:48 +0530

    General First commit

---
+++ src/main/java/org/nokia/Application.java
@@ -0,0 +1,14 @@
+package org.nokia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("First line of code before any commit");
+	}
+
+}
