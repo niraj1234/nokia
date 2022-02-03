707f9219ef0eb4401ba51bb31d5f0e19e6ba4216
Niraj <niraj.webtech@gmail.com>
2022-02-03 17:42:27 +0530

    Adding new API for getting single Product based on product key.
    /product/{productName}

--- src/main/java/org/nokia/ProductController.java
+++ src/main/java/org/nokia/ProductController.java
@@ -1,11 +1,14 @@
 package org.nokia;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,4 +36,29 @@ public class ProductController {
 
 		return new ResponseEntity<List<ProductVO>>(productList,HttpStatus.OK);
 	}	
+	
+	
+	@RequestMapping("/product/{pName}")
+	public ResponseEntity<ProductVO> getProductByName(@PathVariable("pName") String prodName ){
+        HashMap<String,ProductVO> productMap = new HashMap<String,ProductVO>();
+
+		ProductVO p1 = new ProductVO(1, "Mi Mobile", 12223);
+		ProductVO p2 = new ProductVO(2, "Dell Inspiron", 67223);
+		ProductVO p3 = new ProductVO(3, "Oppo Watch", 5666);
+		ProductVO p4 = new ProductVO(4, "Samsung TV", 77272);
+		ProductVO p5 = new ProductVO(5, "DJI Drone Mini", 45090);
+		ProductVO p6 = new ProductVO(6, "GoPro Hero 5", 27009);
+
+		productMap.put("mobile", p1);
+		productMap.put("laptop", p2);
+		productMap.put("watch", p3);
+		productMap.put("tv", p4);
+		productMap.put("drone", p5);
+		productMap.put("camera", p6);
+		
+        ProductVO product = productMap.get("prodName");
+		
+		return new ResponseEntity<ProductVO>(product , HttpStatus.OK);
+
+	}
 }
