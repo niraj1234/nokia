69e6b7e2df74746d0c3395fc57a3c7d716c3cfa8
Niraj <niraj.webtech@gmail.com>
2022-07-23 00:42:55 +0530

    AWS RDS Implementation Adding BookAPI Adding JPA

---
+++ src/main/java/org/nokia/Book.java
@@ -0,0 +1,83 @@
+package org.nokia;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "book_tbl")
+public class Book {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String bookName;
+	private String author;
+	private double price;
+	private int publicationYear;
+
+	public Book() {
+		super();
+	}
+	
+	public Book(int id, String bookName, String author, double price, int publicationYear) {
+		super();
+		this.id = id;
+		this.bookName = bookName;
+		this.author = author;
+		this.price = price;
+		this.publicationYear = publicationYear;
+	}
+	
+	public Book(String bookName, String author, double price, int publicationYear) {
+		super();
+		this.bookName = bookName;
+		this.author = author;
+		this.price = price;
+		this.publicationYear = publicationYear;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", bookName=" + bookName + ", author=" + author + ", price=" + price
+				+ ", publicationYear=" + publicationYear + "]";
+	}
+	
+	
+	
+}
---
+++ src/main/java/org/nokia/BookController.java
@@ -0,0 +1,40 @@
+package org.nokia;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to Book REST API Service !!";
+	}
+	
+	@GetMapping("/book")
+	public List<Book> getBooks() {
+		return bookRepository.findAll();
+	}
+
+	@GetMapping("/book/{id}")
+	public Book getBookById(@PathVariable("id") int id ) {
+		System.out.println("Book kID Requested => " + id);
+		return bookRepository.findById(id).get() ;
+	}
+
+	
+	@PostMapping("/book")
+	public Book addBook(@RequestBody Book book ) {
+		return bookRepository.save(book);
+	}
+	
+}
