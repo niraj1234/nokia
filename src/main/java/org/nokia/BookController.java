c646a9055e214603d7b5df84dac47b62a6ca076f
Niraj <niraj.webtech@gmail.com>
2022-09-20 16:40:19 +0530

    Removing Book Module and AWS RDS connection

--- src/main/java/org/nokia/BookController.java
+++ src/main/java/org/nokia/BookController.java
@@ -9,32 +9,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BookController {
 
-	@Autowired
-	private BookRepository bookRepository;
-	
-	@GetMapping("/")
-	public String welcome() {
-		return "Welcome to Book REST API Service !!";
-	}
-	
-	@GetMapping("/book")
-	public List<Book> getBooks() {
-		return bookRepository.findAll();
-	}
-
-	@GetMapping("/book/{id}")
-	public Book getBookById(@PathVariable("id") int id ) {
-		System.out.println("Book kID Requested => " + id);
-		return bookRepository.findById(id).get() ;
-	}
-
-	
-	@PostMapping("/book")
-	public Book addBook(@RequestBody Book book ) {
-		return bookRepository.save(book);
-	}
-	
-}
+/*
+ * @RestController public class BookController {
+ * 
+ * @Autowired private BookRepository bookRepository;
+ * 
+ * @GetMapping("/") public String welcome() { return
+ * "Welcome to Book REST API Service !!"; }
+ * 
+ * @GetMapping("/book") public List<Book> getBooks() { return
+ * bookRepository.findAll(); }
+ * 
+ * @GetMapping("/book/{id}") public Book getBookById(@PathVariable("id") int id
+ * ) { System.out.println("Book kID Requested => " + id); return
+ * bookRepository.findById(id).get() ; }
+ * 
+ * 
+ * @PostMapping("/book") public Book addBook(@RequestBody Book book ) { return
+ * bookRepository.save(book); }
+ * 
+ * }
+ */
--- src/main/java/org/nokia/BookRepository.java
+++ src/main/java/org/nokia/BookRepository.java
@@ -3,6 +3,12 @@ package org.nokia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface BookRepository extends JpaRepository<Book, Integer>{
+/*
+ * public interface BookRepository extends JpaRepository<Book, Integer>{
+ * 
+ * }
+ */
+
+public interface BookRepository{
 	
 }
